b761aac46d15cd3073e9d636a61c6fa78ccb2ce1
Fábrica Formiga - João Paulo <fabrica.formiga@basis.com.br>
2020-12-22 20:44:56 -0300

    Primeiro commit, iniciado o projeto

---
+++ voila-backend/src/main/java/br/com/voila/backend/voilabackend/VoilaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.voila.backend.voilabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoilaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoilaBackendApplication.class, args);
+	}
+
+}
---
+++ voila-backend/src/test/java/br/com/voila/backend/voilabackend/VoilaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.voila.backend.voilabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoilaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
