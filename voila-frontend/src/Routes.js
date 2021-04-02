5d94588b218447dd7e4f335a1ba1ef4cb6b6eb4b
Fábrica Formiga - João Paulo <fabrica.formiga@basis.com.br>
2021-04-02 15:27:48 -0300

    Ajustado a pagina de recuperacao de senha, login e registro

--- voila-frontend/src/Routes.js
+++ voila-frontend/src/Routes.js
@@ -3,14 +3,16 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './client/pages/Home';
 import Login from './client/pages/Login';
 import Register from './client/pages/Register';
+import ResetPassword from './client/pages/ResetPassword';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home}/>
-                <Route path="/login" component={Login}/>
-                <Route path="/register" component={Register}/>
+                <Route path="/client/login" component={Login}/>
+                <Route path="/client/register" component={Register}/>
+                <Route path="/client/recovery" component={ResetPassword}/>
             </Switch>
         </BrowserRouter>
     )
